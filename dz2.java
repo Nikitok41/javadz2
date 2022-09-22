bf9ac433cfbcc9d6c9e34db78ba54ea064f5e0bd
Nikitok41 <nik777pups@gmail.com>
2022-09-22 06:41:59 +0300

    dz2zadacha1

---
+++ dz2.java
@@ -0,0 +1,22 @@
+// // Задание 1
+// // Напишите программу, записывающую 100 раз слово ”ТЕST” в файл. Слова должны /n 
+// чередоваться по формату – четная итерация большими буквами, /n 
+// нечетные – маленькими Пример: TESTtestTESTtestTEST…
+
+import java.util.logging.Logger;
+
+public class dz2 {
+    public static void main(String[] args) {
+        StringBuilder t = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                
+                t.append("TEST".toUpperCase()); 
+            }
+            else {
+                t.append("test".toLowerCase());
+            }          
+        }
+        System.out.println(t); 
+    }
+}
